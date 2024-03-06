e1008b128506c340440242530650884411db5d07
Qusai963 <qusaiarmoush610@gmail.com>
2024-03-06 16:50:04 +0300

    remove dummy comments

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
-
-  // app.use(helmet());
 
   const appService = app.get(AppService);
   await appService.seed();
@@ -18,18 +15,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe());
-
-  // app.use(function (req, res, next) {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
-  //   res.header(
-  //     'Access-Control-Allow-Headers',
-  //     'Origin, X-Requested-With, Content-Type, Accept',
-  //   );
-  //   next();
-  // });
-
-  //app.useStaticAssets(join(__dirname, '..', '..', 'public-flutter'));
 
   //  app.useGlobalFilters(new CustomExceptionFilter());
   await app.listen(process.env.PORT || 3333);
