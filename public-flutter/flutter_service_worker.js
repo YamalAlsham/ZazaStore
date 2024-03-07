cc9b530654172813c866f813caf13383beee0dd2
Bassam <jawishbassam@gmail.com>
2024-03-07 05:30:30 +0300

    sole errors

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -339,9 +339,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "8ecf2342094b3c5f8f3d5ce4cb18de42
 "icons/ms-icon-150x150.png": "8f03b22258d962f1b68cd66424ab0594",
 "icons/ms-icon-310x310.png": "d23f868b58d8a27e4325bac08f8d2dd7",
 "icons/ms-icon-70x70.png": "b721f9737d490f6a9162748995fa72d9",
-"index.html": "85734b6b871463b346289a1055130d8f",
-"/": "85734b6b871463b346289a1055130d8f",
-"main.dart.js": "0c3017c6d98696e20c9abbaa98d2e23f",
+"index.html": "1592eb685a02ca340798ec090b6bab3c",
+"/": "1592eb685a02ca340798ec090b6bab3c",
+"main.dart.js": "cfcf95de49d23a8ce86002e8eb75efed",
 "manifest.json": "6bb1326374de899bfe0d04cf2e4ddc0c",
 "version.json": "334786c1e21c586a55136a52079bbe81"};
 // The application shell files that are downloaded before a service worker can
