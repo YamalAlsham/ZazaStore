9cd5d4d944c2dc80e5a8da355781cbd4314118ad
Qusai963 <qusaiarmoush610@gmail.com>
2024-03-08 04:10:53 +0300

    .

--- src/core/helpers/sort.helper.ts
+++ src/core/helpers/sort.helper.ts
@@ -12,9 +12,9 @@ export function getOrderByCondition(sort: string): OrderByCondition {
 export function getOrderProductByCondition(sort: string): OrderByCondition {
   switch (sort) {
     case 'newest':
-      return { 'product.id': 'DESC' };
+      return { 'product.createdAt': 'DESC' };
     case 'oldest':
-      return { 'product.id': 'ASC' };
+      return { 'product.createdAt': 'ASC' };
     default:
       return {};
   }
